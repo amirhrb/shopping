57f41081986415dc4205275a37090d236b730b7b
amirhrb <amirrabi70@gmail.com>
2022-09-03 17:40:33 +0430

    parallax resp

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Route, Switch } from "react-router-dom";
 import { ParallaxBanner } from "react-scroll-parallax";
+import { Helmet } from "react-helmet";
 
 //styles
 import styles from "./styles/Homepage.module.css";
@@ -18,62 +19,71 @@ import hero from "../images/will-rust.png";
 const Homepage = (props) => {
   const [isLoaded, setLoaded] = useState({ hero: false, heroBg: false });
   return (
-    <ParallaxBanner
-      layers={[
-        {
-          children: (
-            <img
-              src={heroBg}
-              alt=""
-              className={styles.heroBg}
-              style={{
-                display: isLoaded.hero && isLoaded.heroBg ? "block" : "none",
-              }}
-              onLoad={() => setLoaded({ ...isLoaded, hero: true })}
-            />
-          ),
-          speed: -20,
-        },
-        {
-          children: (
-            <img
-              src={hero}
-              alt="hero bg man standing on mountain"
-              className={styles.hero}
-              style={{
-                display: isLoaded.hero && isLoaded.heroBg ? "block" : "none",
-              }}
-              onLoad={() => setLoaded({ ...isLoaded, heroBg: true })}
-            />
-          ),
-          speed: -10,
-        },
-      ]}
-    >
-      <img
-        src={heroBgDemo}
-        className={styles.heroBgDemo}
-        alt="hero bg man standing on mountain"
-        style={{
-          display: !isLoaded.hero && !isLoaded.heroBg ? "block" : "none",
-        }}
-      />
-      <div className={styles.mainpart}>
-        <div className={styles.centered}>
-          <Switch>
-            <Route
-              path="/home/login"
-              render={(props) => <Login {...props} />}
-            />
-            <Route
-              path="/home/signup"
-              render={(props) => <Signup {...props} />}
-            />
-            <Route path="/home" component={Links} />
-          </Switch>
+    <>
+      <Helmet>
+        <meta
+          name="description"
+          content="here is homepage with parallax hero!"
+        />
+        <title>home</title>
+      </Helmet>
+      <ParallaxBanner
+        layers={[
+          {
+            children: (
+              <img
+                src={heroBg}
+                alt=""
+                className={styles.heroBg}
+                style={{
+                  display: isLoaded.hero && isLoaded.heroBg ? "block" : "none",
+                }}
+                onLoad={() => setLoaded({ ...isLoaded, hero: true })}
+              />
+            ),
+            speed: -20,
+          },
+          {
+            children: (
+              <img
+                src={hero}
+                alt="hero bg man standing on mountain"
+                className={styles.hero}
+                style={{
+                  display: isLoaded.hero && isLoaded.heroBg ? "block" : "none",
+                }}
+                onLoad={() => setLoaded({ ...isLoaded, heroBg: true })}
+              />
+            ),
+            speed: -10,
+          },
+        ]}
+      >
+        <img
+          src={heroBgDemo}
+          className={styles.heroBgDemo}
+          alt="hero bg man standing on mountain"
+          style={{
+            display: !isLoaded.hero && !isLoaded.heroBg ? "block" : "none",
+          }}
+        />
+        <div className={styles.mainpart}>
+          <div className={styles.centered}>
+            <Switch>
+              <Route
+                path="/home/login"
+                render={(props) => <Login {...props} />}
+              />
+              <Route
+                path="/home/signup"
+                render={(props) => <Signup {...props} />}
+              />
+              <Route path="/home" component={Links} />
+            </Switch>
+          </div>
         </div>
-      </div>
-    </ParallaxBanner>
+      </ParallaxBanner>
+    </>
   );
 };
 
--- src/components/Info.js
+++ src/components/Info.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React from "react";
+import { Helmet } from "react-helmet";
 
 const Info = (props) => {
-
-    return (
-        <div>
-        
-        </div>
-    );
+  return (
+    <>
+      <Helmet>
+        <meta name="description" content="here is some more information" />
+        <title>info</title>
+      </Helmet>
+      <div>
+        <h3>some Lorem imsup about me</h3>
+      </div>
+    </>
+  );
 };
 
 export default Info;
